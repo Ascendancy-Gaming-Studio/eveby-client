0f6d61fa9407b7d86453c3604a4019fdb2b03bc3
Julio Cesar <jcmljunior@proton.me>
2024-06-08 20:44:45 -0300

    implementação de comando ping-pong

--- src/features/events/message-create.ts
+++ src/features/events/message-create.ts
@@ -1,8 +1,16 @@
+import { ClientEvents, Message } from 'discord.js';
 import { Event } from '../event-manager/event';
 
 export class MessageCreate extends Event {
   public constructor() {
     super({ name: 'messageCreate' });
   }
-  public async run(...args: any[]): Promise<void> {}
+
+  public async run(client: ClientEvents, message: Message): Promise<void> {
+    if (!message.content.startsWith(this.config?.localStorage.get('prefix')?.getValue())) return
+
+    if (message.content.slice(1) === 'ping') {
+      message.reply('pong');
+    }
+  }
 }
